7f4031ec8aa9ae934848f9a33ffc324820033629
BarkleyRhoat <barkleyrhoat@gmail.com>
2026-05-06 19:30:00 -0400

    added practice.js

---
+++ practice.js
@@ -0,0 +1,110 @@
+// Variables
+var neverUse = true; // should avoid using var
+const defaultVar = "Use first"; // const value cannot be changed.
+let secondaryVar = "Use second"; // you are able to change value/
+
+
+
+
+
+//-----------------------------------------------------
+
+
+
+
+
+// Data types
+const str = "string"; // String: Sequence of characters wrapped in ""
+const num = 1; // Number: Integers
+const bool = true; // Boolean: Two values true or false/ 1 or 0
+const obj = { type: "Object" }; // Object: Stores data with key-values
+const nullValue = null; // NUlL: When we want the variable to intentionally show nothing.
+// const undefined = variable that doesn't have a value
+
+
+
+
+// -------------------------------------------------------
+
+
+
+
+
+// Conditional Statements
+
+
+
+
+// Function
+function importantConcept(value) {
+  console.log("JavaScripts backbone are " + value);
+}
+importantConcept("functions");
+
+function templateString(ts) {
+  console.log(`This is called a ${ts}`); // template string example
+}
+templateString("template string");
+
+
+
+
+
+// --------------------------------------------------------
+
+
+
+
+
+// arrow function
+const square = n => n * n;
+console.log(square(10)); // first n is parameter everything after arrow is body. 
+
+const isDog = (name) => name === "Hamilton"; // does name ==== name 
+console.log(isDog("Hamilton")); // true 
+console.log(isDog("Rex"));      // false
+
+
+
+
+// --------------------------------------------------------
+
+
+
+
+
+// Scope
+
+
+
+
+
+
+// Array
+const myArray = ["Jim", "Bob", "Frank", "Barkley", "Keira"]; // array with strings
+console.log(myArray)
+
+const numArray = [1, 2, 3, 4, 5]; // array with numbers
+numArray() // This is how we call the variable. 
+
+const ppl = ['bark', 'kiki', 'hamham', 'charchar']
+console.log(ppl[2]) // this is how we access elements (argument[element])
+
+
+
+
+// ------------------------------------------------------
+
+
+
+
+
+
+// Array iterations (Methods)
+
+
+// String Methods
+
+// Looping
+
+// Operators
