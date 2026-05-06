1f218e9ae59f5fb9713ff0ac617c2fc606f95057
BarkleyRhoat <barkleyrhoat@gmail.com>
2026-05-06 13:09:27 -0400

    created .reduce method file reduceMethod.js

---
+++ reduceMethod.js
@@ -0,0 +1,37 @@
+let books = [
+  {
+    title: "Moby Dick",
+    price: 20.0,
+    inventory: 10,
+  },
+  {
+    title: "Hells Angels",
+    price: 22.0,
+    inventory: 45,
+  },
+  {
+    title: "Cat in the Hat",
+    price: 23.0,
+    inventory: 100,
+  },
+  {
+    title: "Dune",
+    price: 50.0,
+    inventory: 1,
+  },
+];
+
+// const reducer = (accumulator, item) => {
+//     console.log(accumulator, item)
+//   let total = item.price * item.inventory;
+//   return (accumulator += total);
+// };
+
+// let total = books.reduce(reducer, 0);
+
+const reducer = (accumulator, item) => {
+    console.log(accumulator, item)
+    return accumulator +", " + item.title
+}
+
+let total = books.reduce(reducer, '').substring(2)
