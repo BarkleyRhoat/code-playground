026bd9052c94c496ba56068fa21897a299093e1d
BarkleyRhoat <barkleyrhoat@gmail.com>
2026-05-11 11:40:33 -0400

    added src with query selector

--- DOM/changingDomElements.js
+++ DOM/changingDomElements.js
@@ -4,3 +4,12 @@ function h1Element(bold) {
   h1.style.color = "black";
 }
 h1Element();
+
+function addImage(img_url) {
+  let img = document.querySelector("img");
+  img.src = img_url;
+}
+addImage(
+  "https://duckduckgo.com/?t=ffab&q=funny+photos&ia=images&iax=images&iai=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F1629781%2Fpexels-photo-1629781.jpeg%3Fcs%3Dsrgb%26dl%3Dpexels-ilargian-faus-763704-1629781.jpg%26fm%3Djpg",
+);
+
